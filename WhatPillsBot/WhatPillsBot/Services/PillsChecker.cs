9ddbf5db3de9c8530a8fda56cce94ec2eba19393
VladKRP <karpovichvladislavitsp@gmail.com>
2017-06-09 12:05:46 +0300

    Add services

--- WhatPillsBot/WhatPillsBot/Services/PillsChecker.cs
+++ WhatPillsBot/WhatPillsBot/Services/PillsChecker.cs
@@ -10,8 +10,20 @@ namespace WhatPillsBot.Services
     {
         public static IEnumerable<Pill> GetPills()
         {
-            string url = "https://pill-id.webpoisoncontrol.org/#/tab-imprint";
+            string url = "https://pill-id.webpoisoncontrol.org/js/pill-id.min.js";
             var site = SiteParser.Parser.ParseSiteAsString(url);
+            return null;
+        }
+
+        public static IEnumerable<string> GetExistingPillColors()
+        {
+            string url = "https://pill-id.webpoisoncontrol.org/js/pill-id.min.js";
+            return null;
+        }
+
+        public static IEnumerable<string> GetExistingPillShapes()
+        {
+            string url = "https://pill-id.webpoisoncontrol.org/js/pill-id.min.js";
             return null;
         }
     }
